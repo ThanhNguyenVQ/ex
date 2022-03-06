6f10e3978a328ad0405cab2b60b01a1b5f23e7bf
dtng2030 <100981082+dtng2030@users.noreply.github.com>
2022-03-07 01:03:42 +0700

    Update abc.cpp

--- abc.cpp
+++ abc.cpp
@@ -4,10 +4,10 @@ using namespace std;
 int main()
 {
     int a,b,c,d;
-    cout << "Nhap a: "; cin >> a;
-    cout << "\nNhap b: "; cin >> b;
-    cout << "\nNhap c: "; cin >> c;
-    cout << "\nNhap d: "; cin >> d;
+    cout << "Nhap so a: "; cin >> a;
+    cout << "\nNhap so b: "; cin >> b;
+    cout << "\nNhap so c: "; cin >> c;
+    cout << "\nNhap so d: "; cin >> d;
     cout << "\nBon so vua nhap la: " << a << ' ' << b << ' ' << c << ' ' << d;
     return 0;
 }
